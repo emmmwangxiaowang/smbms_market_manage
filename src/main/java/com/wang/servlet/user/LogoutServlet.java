b5b21c225f992478b65e5ba6aecbe55ba871df77
emmmwangxiaowang <954544828@qq.com>
2021-04-21 12:20:10 +0800

    1.jdbc 2.mvc三层架构下登录操作 3.登录校验

---
+++ src/main/java/com/wang/servlet/user/LogoutServlet.java
@@ -0,0 +1,29 @@
+package com.wang.servlet.user;
+
+import com.wang.util.Constants;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 王航
+ * @QQ 954544828
+ * @since 2021/4/21 0021
+ */
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //移除用户session
+        req.getSession().removeAttribute(Constants.USER_SESSION);
+        //返回登录页面
+        resp.sendRedirect(req.getContextPath()+"/login.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
